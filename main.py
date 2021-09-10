2cccc9524804693f66c2c1c0df26fe154f090fc0
VladimirGuillochet <vladimir.guillochet@ntymail.com>
2021-09-10 14:42:55 +0200

    mise en place lignes 2D

--- main.py
+++ main.py
@@ -8,8 +8,8 @@ class MainWidget(Widget):
     perspective_point_x = NumericProperty(0)
     perspective_point_y = NumericProperty(0)
 
-    VERTICAL_NUMBER_LINES = 7
-    VERTICAL_LINES_SPACING = 0.1 # pourcentage selon largeur ecran
+    VERTICAL_NUMBER_LINES = 7  # nb ligne impaire pour line milieu centrée
+    VERTICAL_LINES_SPACING = 0.1  # pourcentage selon largeur ecran
     vertical_lines = []
 
     def __init__(self,**kwargs):
@@ -38,10 +38,22 @@ class MainWidget(Widget):
         """ definition lignes verticales - objet a variables dynamiques donc traité dans le py"""
         with self.canvas:
             Color(1, 1, 1)
-            self.line = Line(points=[100, 0, 100, 100])
+            # self.line = Line(points=[100, 0, 100, 100])
+            for line in range(0, self.VERTICAL_NUMBER_LINES):
+                self.vertical_lines.append(Line())  # ajout des lignes verticales
 
     def update_vertical_lines(self):
-        self.line.points = [self.perspective_point_x, 0, self.perspective_point_x, 100]
+        # self.line.points = [self.perspective_point_x, 0, self.perspective_point_x, 100]
+        central_line_x = self.width / 2
+        spacing_line_x = self.VERTICAL_LINES_SPACING * self.width
+        offset_line = -int(self.VERTICAL_NUMBER_LINES / 2)  # decalage inter ligne
+        for line in range(0, self.VERTICAL_NUMBER_LINES):
+            x1 = int(central_line_x + offset_line * spacing_line_x)
+            y1 = 0
+            x2 = x1
+            y2 = self.height
+            self.vertical_lines[line].points = [x1, y1, x2, y2]
+            offset_line += 1
 
 
 class GalaxyApp(App):

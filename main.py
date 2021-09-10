1c23590c7794b499d10b0de4921b353802abef23
VladimirGuillochet <vladimir.guillochet@ntymail.com>
2021-09-10 14:24:01 +0200

    definition d'une ligne dans un premier temps pour essai

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from kivy.app import App
+from kivy.graphics import Color, Line
 from kivy.properties import NumericProperty
 from kivy.uix.widget import Widget
 
@@ -7,9 +8,13 @@ class MainWidget(Widget):
     perspective_point_x = NumericProperty(0)
     perspective_point_y = NumericProperty(0)
 
+    VERTICAL_NUMBER_LINES = 7
+    VERTICAL_LINES_SPACING = 0.1 # pourcentage selon largeur ecran
+    vertical_lines = []
+
     def __init__(self,**kwargs):
         super().__init__(**kwargs)
-        # print(f"Init width : {self.width} - height : {self.height}")
+        self.init_vertical_lines()
 
     def on_parent(self, widget, parent):
         # print(f"Init width : {self.width} - height : {self.height}")
@@ -20,7 +25,7 @@ class MainWidget(Widget):
         # print(f"Init width : {self.width} - height : {self.height}")
         # self.perspective_point_x = self.width / 2
         # self.perspective_point_y = self.height * 0.75
-        pass
+        self.update_vertical_lines()
 
     def on_perspective_point_x(self, widget, value):
         """ on_perspective_point_x permet d'appeler directement les variables perspective_point_x """
@@ -29,8 +34,18 @@ class MainWidget(Widget):
     def on_perspective_point_y(self, widget, value):
         print(f"PY : {value}")
 
+    def init_vertical_lines(self):
+        """ definition lignes verticales - objet a variables dynamiques donc traité dans le py"""
+        with self.canvas:
+            Color(1, 1, 1)
+            self.line = Line(points=[100, 0, 100, 100])
+
+    def update_vertical_lines(self):
+        self.line.points = [self.perspective_point_x, 0, self.perspective_point_x, 100]
+
 
 class GalaxyApp(App):
     pass
+
 
 GalaxyApp().run()

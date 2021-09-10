621fdd7a9b6957ab95f1d2275bc1f4aaf8d0cb3f
VladimirGuillochet <vladimir.guillochet@ntymail.com>
2021-09-10 12:18:47 +0200

    add kivy

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
-print("hello world !")
-print("aa")
+from kivy.app import App
+from kivy.uix.widget import Widget
+
+
+class MainWidget(Widget):
+    pass
+
+class GalaxyApp(App):
+    pass
+
+GalaxyApp().run()

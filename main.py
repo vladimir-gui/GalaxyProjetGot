03f1d0f866e33d39fbec510fa5cc7ea76452b4ef
VladimirGuillochet <vladimir.guillochet@ntymail.com>
2021-09-13 11:53:28 +0200

    mise en place rafraichissement 60 fps du jeu

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from kivy.app import App
 from kivy.graphics import Color, Line
-from kivy.properties import NumericProperty
+from kivy.properties import NumericProperty, Clock
 from kivy.uix.widget import Widget
 
 
@@ -20,25 +20,29 @@ class MainWidget(Widget):
         super().__init__(**kwargs)
         self.init_vertical_lines()
         self.init_horizontal_lines()
+        Clock.schedule_interval(self.update, 1.0 / 60.0)  # rafraichissement 60 fps
 
     def on_parent(self, widget, parent):
         # print(f"Init width : {self.width} - height : {self.height}")
         pass
 
     def on_size(self, *args):
-        """ Taille fenetre temps reel """
-        # print(f"Init width : {self.width} - height : {self.height}")
-        # self.perspective_point_x = self.width / 2
-        # self.perspective_point_y = self.height * 0.75
-        self.update_vertical_lines()
-        self.update_horizontal_lines()
+        pass
+        # """ Taille fenetre temps reel """
+        # # print(f"Init width : {self.width} - height : {self.height}")
+        # # self.perspective_point_x = self.width / 2
+        # # self.perspective_point_y = self.height * 0.75
+        # self.update_vertical_lines()
+        # self.update_horizontal_lines()
 
     def on_perspective_point_x(self, widget, value):
-        """ on_perspective_point_x permet d'appeler directement les variables perspective_point_x """
-        print(f"PX : {value}")
+        pass
+        # """ on_perspective_point_x permet d'appeler directement les variables perspective_point_x """
+        # print(f"PX : {value}")
 
     def on_perspective_point_y(self, widget, value):
-        print(f"PY : {value}")
+        pass
+        # print(f"PY : {value}")
 
     def init_vertical_lines(self):
         """ definition lignes verticales - objet a variables dynamiques donc traité dans le py"""
@@ -83,8 +87,8 @@ class MainWidget(Widget):
 
     def transform(self, x, y):
         """choix affichage 2D ou perspective"""
-        # return self.transform_2D(x, y)
-        return self.transform_perspective(x, y)
+        return self.transform_2D(x, y)
+        # return self.transform_perspective(x, y)
 
     def transform_2D(self, x, y):
         """choix affichage 2D"""
@@ -104,6 +108,9 @@ class MainWidget(Widget):
         transfor_y = self.perspective_point_y - factor_y * self.perspective_point_y
 
         return int(transfor_x), int(transfor_y)
+
+    def update(self, dt):
+        print("up")
 
 
 class GalaxyApp(App):
